f010b2cd3f56bd4b9de4741b68f00578d2620530
batchu <batchu@batchu.net>
2016-09-11 21:36:35 -0600

    Mopub banner now initialized. Works only with API 22 (5.0.1) only and not on Marshmellow

--- app/src/main/java/com/example/app/myapplication/MainActivity.java
+++ app/src/main/java/com/example/app/myapplication/MainActivity.java
@@ -3,11 +3,20 @@ package com.example.app.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mopub.mobileads.MoPubView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MoPubView moPubView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        moPubView = (MoPubView) findViewById(R.id.adview);
+        moPubView.setAdUnitId("c9c2ea9a8e1249b68496978b072d2fd2"); // Enter your Ad Unit ID from www.mopub.com
+        moPubView.loadAd();
     }
 }
